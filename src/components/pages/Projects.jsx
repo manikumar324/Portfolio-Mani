7be27854de1f6d3df28dac5f4469fd2a9ab6e473
ManiKumar Nakka <manikumarnakka324@gmail.com>
2025-09-25 11:47:40 +0530

    projects section updated

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import swyft from '../../assets/swyft-project.png';
 import todo from '../../assets/todolist.png';
 import crud from '../../assets/crudoperations.png';
-import chatapp from '../../assets/chat-app.png';
+import blog from '../../assets/blog.png';
 
 const Projects = () => {
 
@@ -36,14 +36,14 @@ const Projects = () => {
                     </div>
                 </div>
                 <div className="project-image1 rounded my-3">
-                    <img className="image2" src={crud} alt="crud" />
-                    <p className="text-color my-1">Node.js , Express.js & SQL</p>
-                    <h6>CRUD OPERATIONS</h6>
+                    <img className="image2" src={todo} alt="todo" />
+                    <p className="text-color my-1">React.js & Tailwind Css</p>
+                    <h6>TO - DO LIST APPLICATION</h6>
                     <div className="d-flex justify-content-center">
                         <button
                             onClick={() =>
                             handleNavigate(
-                                "https://github.com/manikumar324/crud_operations"
+                                "https://github.com/manikumar324/todo-application-using-REACT"
                             )
                             }
                             className="src-button"
@@ -55,14 +55,14 @@ const Projects = () => {
             </div>
             <div>
                 <div className="project-image1 rounded">
-                    <img className="image2" src={todo} alt="todo" />
-                    <p className="text-color my-1">React.js & Tailwind Css</p>
-                    <h6>TO - DO LIST APPLICATION</h6>
+                    <img className="image2" src={blog} alt="blog" />
+                    <p className="text-color my-1">React.js, Node.js, Express.js, Mongo db & Tailwind Css</p>
+                    <h6>BLOG APPLICATION</h6>
                         <div className="d-flex justify-content-center">
                             <button
                                 onClick={() =>
                                 handleNavigate(
-                                    "https://github.com/manikumar324/todo-application-using-REACT"
+                                    "https://github.com/manikumar324/Blog-Application"
                                 )
                                 }
                                 className="src-button"
@@ -72,14 +72,14 @@ const Projects = () => {
                         </div>
                 </div>
                 <div className="project-image2 rounded my-3">
-                    <img className="image2" src={chatapp} alt="chatapp" />
-                    <p className="text-color my-1">React.js, Node.js, Express.js, Tailwind Css</p>
-                    <h6>CHAT - APPLICATION</h6>
+                    <img className="image2" src={crud} alt="crud" />
+                    <p className="text-color my-1">Node.js , Express.js & SQL</p>
+                    <h6>CRUD OPERATIONS</h6>
                     <div className="d-flex justify-content-center">
                         <button
                             onClick={() =>
                             handleNavigate(
-                                "https://github.com/manikumar324/Chat-App"
+                                "https://github.com/manikumar324/crud_operations"
                             )
                             }
                             className="src-button"
